80e8c1c5257bc65d35deedcec836bd161257d01d
Merna elseady <78154455+Mernamohamed44@users.noreply.github.com>
2024-07-09 14:44:01 +0300

    home screen-done

--- lib/features/home/components/tab_bar_views.dart
+++ lib/features/home/components/tab_bar_views.dart
@@ -29,7 +29,7 @@ class TabBarWidgetViews extends StatelessWidget {
           ),
         ],
         pages:  [
-          SearchScreenViews(),
+          CustomBuyWidget(),
           CustomBuyWidget(),
         ],
         isScrollable: false,
